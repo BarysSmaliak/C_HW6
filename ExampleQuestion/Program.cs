0f0227e5a09805c6eba739f1c07d5fa96173ac35
Barys Smaliak <smboris@yandex.ru>
2023-04-17 11:40:33 +0300

    Example added

---
+++ ExampleQuestion/Program.cs
@@ -0,0 +1,22 @@
+﻿// Попытка сделать ввод чисел от Пользователя, где условием окончания ввода будет нажатие символа "N"
+// Пока не введено "N", просто считаю сумму введенных чисел. Можно вместо этого запустить какую-то функцию,
+// например, подсчёт положительных чисел.
+
+void EnterMumbers ()
+{
+    string a = String.Empty;
+    int num;
+    int sum = 0;
+    while (!(a == "N")) // Вот тут, по моему, что-то не то. 
+    {
+        Console.WriteLine
+        ("Input number or input N if you wont to finish");
+        int.TryParse(Console.ReadLine(), out num);
+        sum += num;
+        
+    }
+    Console.WriteLine(sum);
+}
+
+EnterMumbers();
+
