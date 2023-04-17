fdefe6989a12f4297d3848a3808333cd44576d9d
Barys Smaliak <smboris@yandex.ru>
2023-04-17 13:26:48 +0300

    Example001 changed

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -2,3 +2,24 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 
+void PositiveNumbers ()
+{
+    string a = String.Empty;
+    int num;
+    int sum = 0; 
+    while (!(a == "N"))
+    {
+        Console.WriteLine
+        ("Input number or input N if you wont to finish");
+        a = Console.ReadLine();
+        if (int.TryParse(a, out num))
+        {
+            sum += 1;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine
+    ($"The number of entered numbers that are greater than 0 is equal to {sum}");
+}
+
+PositiveNumbers();
